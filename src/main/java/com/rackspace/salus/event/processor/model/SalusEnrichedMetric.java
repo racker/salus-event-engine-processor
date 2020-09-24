d0e4c55508a18f92405738a9bb7153ef9c741942
James Buchan <james.buchan@rackspace.com>
2020-09-24 16:12:39 -0600

    Add initial models

---
+++ src/main/java/com/rackspace/salus/event/processor/model/SalusEnrichedMetric.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 Rackspace US, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rackspace.salus.event.processor.model;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+import lombok.Data;
+
+@Data
+public class SalusEnrichedMetric extends EnrichedMetric {
+  // initial fields are set when ingesting a new metric
+  String resourceId;
+  String monitorId;
+  String zoneId;
+  String monitorType;
+  String monitorSelectorScope;
+  // the below fields are set when the metric matches a Task Query within Esper
+  String taskId;
+  List<String> excludedResourceIds;
+  Instant stateEvaluationTimestamp;
+  Map<String, Integer> expectedStateCounts;
+//  EventState state;
+  String state;
+}
